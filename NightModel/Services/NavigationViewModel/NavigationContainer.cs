ed97aab31ca7af99e7ab2d2b867ae4e1e30f1692
hhypest <hypest3@yandex.ru>
2022-11-04 21:57:51 +0300

    update

---
+++ NightModel/Services/NavigationViewModel/NavigationContainer.cs
@@ -0,0 +1,22 @@
+﻿using NightModel.ViewModel;
+using System;
+
+namespace NightModel.Services.NavigationViewModel;
+
+public abstract class NavigationContainer<T> where T : IRelayViewModel
+{
+    private T? _viewModel;
+
+    public virtual T? ViewModel { get => _viewModel; set => SetViewModel(ref _viewModel, value, OnViewModelChanged); }
+
+    protected event Action? ViewModelChanged;
+
+    protected virtual void OnViewModelChanged()
+        => ViewModelChanged?.Invoke();
+
+    protected virtual void SetViewModel(ref T? field, T? value, Action? onChanged)
+    {
+        field = value;
+        onChanged?.Invoke();
+    }
+}
